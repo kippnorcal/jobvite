265f4c94ba64ae96468f30226a6ee899e9e03cc8
dhess <dan.hess@kippbayarea.org>
2019-01-31 17:10:31 -0800

    Add call to candidates to main for inspecting payload

---
+++ main.py
@@ -0,0 +1,11 @@
+import jobvite
+import os
+
+jobvite_key = os.getenv("JOBVITE_API_KEY")
+jobvite_secret= os.getenv("JOBVITE_API_SECRET")
+
+api = jobvite.JobviteAPI(jobvite_key, jobvite_secret)
+
+candiates = api.candidates()
+
+print(candidates)

2d5d2cd563b534db2ec5370a936abaa6aeb729e1
dhess <dan.hess@kippbayarea.org>
2019-02-13 16:05:47 -0800

    Refactor to compartmentalize request and file writing logic

--- main.py
+++ main.py
@@ -23,16 +23,28 @@ else:
     MODIFIED_DATE = (datetime.now() - timedelta(1)).strftime("%Y-%m-%d")
 
 
+def get_candidates():
+    results = jobvite.JobviteAPI().candidates(modified_date=MODIFIED_DATE)
+    candidates = []
+    for result in results:
+        candidates.append(Candidate(result).__dict__)
+    logging.info(f"Retrieved {len(candidates)} candidate records from Jobvite API")
+    return candidates
+
+
+def write_csv(dataframe, delimiter=","):
+    datestamp = datetime.now().strftime("%Y%m%d%I%M")
+    filename = f"output/candidates_{datestamp}.csv"
+    dataframe.to_csv(filename, sep=delimiter, index=False)
+    logging.info(f"Wrote {len(dataframe.index)} records to {filename}")
+
+
+@elapsed
 def main():
     try:
-        results = jobvite.JobviteAPI().candidates(modified_date=MODIFIED_DATE)
-        candidates = []
-        for result in results:
-            candidates.append(Candidate(result).__dict__)
-
+        candidates = get_candidates()
         df = pd.DataFrame(candidates)
-        datestamp = datetime.now().strftime("%Y%m%d%I%M")
-        df.to_csv(f"output/candidates_{datestamp}.csv", sep=",", index=False)
+        write_csv(df)
 
     except Exception as e:
         logging.critical(e)

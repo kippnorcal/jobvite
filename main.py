3477ff8aaa1e4278ba1295a1351b4378c1f6cbd7
dhess <dan.hess@kippbayarea.org>
2019-02-20 20:26:55 -0800

    DATA-475 Add dataframe column renaming and table loading

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import json
 import logging
 import os
 import sys
+import urllib
 import jobvite
 from timer import elapsed
 import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text as sa_text
 from candidate import Candidate
+
+# Connect to Database
+driver = "{ODBC Driver 13 for SQL Server}"
+ip = os.getenv("SERVER_IP")
+db = os.getenv("DB")
+user = os.getenv("USER")
+pwd = os.getenv("PWD")
+params = urllib.parse.quote_plus(
+    f"DRIVER={driver};SERVER={ip};DATABASE={db};UID={user};PWD={pwd}"
+)
+engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
 
 
 logging.basicConfig(
@@ -44,7 +58,28 @@ def main():
     try:
         candidates = get_candidates()
         df = pd.DataFrame(candidates)
-        write_csv(df)
+        column_map = {
+            "are_you_a_former_or_current_kipp_employee": "formerOrCurrentKIPP",
+            "are_you_an_alumnus_of_a_kipp_school": "KIPPAlumni",
+            "do_you_hold_a_valid_teacher_certification": "validTeacherCert",
+            "do_you_speak_a_language_other_than_english_or_spanish": "otherLanguageSpeaker",
+            "do_you_speak_spanish": "spanishSpeaker",
+            "for_your_next_position_what_would_be_your_desired_salary_range": "desiredSalary",
+            "have_you_applied_to_other_kipp_regions_besides_the_bay_area": "otherKIPPRegions",
+            "how_did_you_hear_about_kipp": "howDidYouHear",
+            "how_many_years_of_fulltime_classroom_teaching_experience_do_you_have_not_including_student_teaching_internships_tutoring_or_volunteer_work": "teachingExperience",
+            "how_many_years_of_fulltime_work_experience_do_you_have": "yrsExperience",
+            "if_you_have_completed_a_teacher_licensure_program_please_indicate_what_type_of_program": "teacherLicensureProgram",
+            "kipp_has_schools_located_in_different_parts_of_the_bay_area__please_select_your_geographic_preferences": "geoPreference",
+            "please_indicate_the_specific_grade_levels_you_would_like_to_teach": "gradePref",
+            "please_indicate_the_specific_subject_areas_you_would_like_to_teach": "subjectPref",
+            "please_specify_which_language": "otherLanguageSpoken",
+            "when_are_you_available_to_begin_work": "workStartAvailability",
+        }
+        df.rename(columns=column_map, inplace=True)
+        df.to_sql(
+            "jobvite_cache", engine, schema="custom", if_exists="append", index=False
+        )
 
     except Exception as e:
         logging.critical(e)

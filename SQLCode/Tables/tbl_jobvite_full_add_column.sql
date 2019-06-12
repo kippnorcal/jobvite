5bebac5f1f9b86ce278071d59d9e1fd12784dc2c
Susan Xiong <susanxiong@KBAS-C1MWJ1FKJ1WK.local>
2019-06-12 13:46:17 -0700

    Add 4 columns for HR account provisioning to SQL code

--- SQLCode/Tables/tbl_jobvite_full_add_column.sql
+++ SQLCode/Tables/tbl_jobvite_full_add_column.sql
@@ -1,3 +1,15 @@
 USE Cust14315
 
 ALTER TABLE custom.jobvite_full ADD [source] VARCHAR(MAX) NULL;
+
+ALTER TABLE custom.jobvite_full
+    ADD [exempt_status] VARCHAR(MAX),
+        [offer_1] VARCHAR(MAX),
+        [offer_2] VARCHAR(MAX),
+        [assigned_pay_location] VARCHAR(MAX)
+
+ALTER TABLE custom.jobvite_cache
+    ADD [exempt_status] VARCHAR(MAX),
+        [offer_1] VARCHAR(MAX),
+        [offer_2] VARCHAR(MAX),
+        [assigned_pay_location] VARCHAR(MAX)

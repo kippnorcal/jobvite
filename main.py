d2644633a2a3f6287219d7377fa36fc4f60d4e03
Mark Young <markyoung.ca@gmail.com>
2024-11-01 11:55:03 -0700

    Updated logic in main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import argparse
 from datetime import datetime, timedelta
+import glob
+import json
 import logging
+import os
 import sys
 import traceback
 
+from gbq_connector import CloudStorageClient
 from job_notifications import create_notifications
 from job_notifications import timer
 import pandas as pd
-from sqlsorcery import MSSQL
 
 from candidate import Candidate
 from data_config import custom_application_fields
 from job import Job
-import jobvite
+from jobvite import JobviteAPI
 from transformations import Field_Transformations
 
 
@@ -33,7 +36,7 @@ parser.add_argument(
     "--start-date",
     help="Start Date - format YYYY-MM-DD",
     dest="start_date",
-    default=(datetime.now() - timedelta(1)).strftime("%Y-%m-%d"),
+    default=None,
 )
 parser.add_argument(
     "--end-date",
@@ -46,25 +49,54 @@ args = parser.parse_args()
 START_DATE = args.start_date
 END_DATE = args.end_date
 
+LOCAL_STORAGE_FOLDER = "./output/"
+CANDIDATES_CLOUD_FOLDER = os.getenv("CANDIDATES_CLOUD_FOLDER")
+JOBS_CLOUD_FOLDER = os.getenv("JOBS_CLOUD_FOLDER")
+BUCKET = os.getenv("BUCKET")
+
 logger = logging.getLogger(__name__)
 
 
-def get_candidates():
-    results = jobvite.JobviteAPI().candidates(start_date=START_DATE, end_date=END_DATE)
-    candidates = []
+def get_candidates(jobvite_con, cloud_client):
+    results = jobvite_con.candidates(start_date=START_DATE, end_date=END_DATE)
+    count = 0
     for result in results:
-        candidates.append(Candidate(result).__dict__)
-    logging.info(f"Retrieved {len(candidates)} candidate records from Jobvite API")
-    return pd.DataFrame(candidates)
+        data = Candidate(result).__dict__
+        create_and_upload_file(data, cloud_client, "candidate_eid", "application_eid", "candidate")
+        count += 1
+    results = jobvite_con.candidates(start_date=START_DATE, end_date=END_DATE)
+    logging.info(f"Retrieved {count} candidate records from Jobvite API")
 
 
-def get_jobs():
-    results = jobvite.JobviteAPI().jobs()
-    jobs = []
+def get_jobs(jobvite_con, cloud_client):
+    results = jobvite_con.jobs()
+    count = 0
     for result in results:
-        jobs.append(Job(result).__dict__)
-    logging.info(f"Retrieved {len(jobs)} job records from Jobvite API")
-    return pd.DataFrame(jobs)
+        data = Job(result).__dict__
+        create_and_upload_file(data, cloud_client, "eId", "requisitionId", "job")
+        count += 1
+    logging.info(f"Retrieved {count} job records from Jobvite API")
+
+
+def create_and_upload_file(data, cloud_client, record_id, primary_key, record_type):
+        eid = data[record_id]
+        pk = data[primary_key]
+        file_name = f"{record_type}_{eid}_{pk}.ndjson"
+        local_file_path = os.path.join(LOCAL_STORAGE_FOLDER, file_name)
+        # Writing file to local dir
+        with open(local_file_path, "w") as f:
+            f.write(json.dumps(data) + "\n")
+        
+        # Uploading file from local dir
+        with open(local_file_path, "r") as f:
+            blob = os.path.join(JOBS_CLOUD_FOLDER, file_name)
+            cloud_client.load_file_to_cloud(BUCKET, blob, f)
+
+
+def cleanup_files():
+    files = glob.glob(f"./output/*.ndjson")
+    for file in files:
+        os.remove(file)
 
 
 def rename_columns(candidates, jobs):
@@ -77,19 +109,18 @@ def rename_columns(candidates, jobs):
 def main():
     notifications = create_notifications("Jobvite", "mailgun", logs="app.log")
     try:
-        candidates = get_candidates()
-        jobs = get_jobs()
-        rename_columns(candidates, jobs)
-        transformed_candidates = Field_Transformations(candidates).dataframe
-        connection = MSSQL()
-        connection.insert_into(
-            "jobvite_cache", transformed_candidates, if_exists="replace"
-        )
-        connection.exec_sproc("sproc_Jobvite_MergeExtract", autocommit=True)
-        connection.insert_into("jobvite_jobs_cache", jobs, if_exists="replace")
-        connection.exec_sproc("sproc_Jobvite_jobs_MergeExtract", autocommit=True)
-        logger.info(f"Loaded {len(candidates.index)} candidates")
-        logger.info(f"Loaded {len(jobs.index)} jobs")
+        jobvite_con = JobviteAPI()
+        cloud_client = CloudStorageClient()
+        logger.info("Getting candidate data")
+        get_candidates(jobvite_con, cloud_client)
+        logger.info("Cleaning up candidate files")
+        cleanup_files()
+
+        logger.info("Getting job data")
+        get_jobs(jobvite_con, cloud_client)
+        logger.info("Cleaning up job files")
+        cleanup_files()
+
         notifications.notify()
     except Exception as e:
         logging.exception(e)
